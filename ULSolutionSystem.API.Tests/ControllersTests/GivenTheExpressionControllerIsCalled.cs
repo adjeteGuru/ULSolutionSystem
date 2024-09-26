62859a99e130c1237bfa95d7810c77abf3b3e0a1
adjeteGuru <justinadjev@hotmail.com>
2024-09-26 23:10:25 +0100

    feat: add method exist test to implement controller ProcessEvaluation

---
+++ ULSolutionSystem.API.Tests/ControllersTests/GivenTheExpressionControllerIsCalled.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using ULSolutionSystem.API.Controllers;
+
+namespace ULSolutionSystem.API.Tests.ControllersTests
+{
+    public class GivenTheExpressionControllerIsCalled
+    {
+        private ExpressionController systemUnderTest;
+
+        public GivenTheExpressionControllerIsCalled()
+        {
+            systemUnderTest = new ExpressionController();
+        }
+
+        [Fact]
+        public void ProcessEvaluation_WhenAParameterIsSuppliedAndTheExpressionIsEvaluated_ThenNoExceptionIsThrown()
+        {
+            var expression = "1";
+            var act = () => systemUnderTest.ProcessEvaluation(expression);
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void ProcessEvaluation_WhenAParameterIsSuppliedAndTheExpressionIsEvaluated_ThenTheExpectedResultTypeIsReturned()
+        {
+            var expression = "1";           
+            var act = systemUnderTest.ProcessEvaluation(expression);
+            act.Should().BeOfType<OkObjectResult>();
+        }
+    }
+}
---
+++ ULSolutionSystem.API/Controllers/ExpressionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ULSolutionSystem.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpressionController : ControllerBase
+    {
+        public ExpressionController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult ProcessEvaluation([FromQuery] string expression)
+        {
+            if (string.IsNullOrEmpty(expression))
+            {
+                return BadRequest("Expression cannot be null or empty.");
+            }
+            return Ok(default);
+        }
+    }
+}
